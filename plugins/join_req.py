ab95d7cd0ca08cff68d61ad0a3fe85d1d918f018
Noob <akhilkrishnaus1234@gmail.com>
2025-08-04 07:53:51 +0530

    Update and rename mnbots.py to join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -20,4 +20,4 @@ async def join_request_handler(client, update: ChatJoinRequest):
 @Client.on_message(filters.command("clear_join_users") & filters.user(ADMINS))
 async def clear_join_users(_, message: Message):
     JOIN_REQUEST_USERS.clear()
-    await message.reply_text("✅ Cleared all join request users.")
+    await message.reply_text("✅ Cleared All Join Request Users.")
