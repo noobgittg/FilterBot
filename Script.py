38215b6b871a1f892cb039ec3a80062fbd38d3db
Noob <akhilkrishnaus1234@gmail.com>
2025-08-04 07:51:21 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,191 +1,175 @@
 class script(object):
-    START_TXT = """ Hᴇʟʟᴏ {}.
-𝖨𝗆 𝖺𝗇 𝖺𝗎𝗍𝗈 𝖿𝗂𝗅𝗍𝖾𝗋 𝖻𝗈𝗍 𝗐𝗁𝗂𝖼𝗁 𝖼𝖺𝗇 𝗉𝗋𝗈𝗏𝗂𝖽𝖾 𝗆𝗈𝗏𝗂𝖾𝗌 𝗂𝗇 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉𝗌.
-+ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 + 𝖺𝗇𝖽 𝗉𝗋𝗈𝗆𝗈𝗍𝖾 𝗆𝖾 𝖺𝗌 𝖺𝖽𝗆𝗂𝗇 𝗍𝗈 𝗅𝖾𝗍 𝗆𝖾 𝗀𝖾𝗍 𝗂𝗇 𝖺𝖼𝗍𝗂𝗈𝗇."""
-    HELP_TXT = """
-     Hey  {} 
-Currently using free server so please Dont kill Me...
-"""
-    ABOUT_TXT = """<b>
-◎ Cʀᴇᴀᴛᴏʀ: <a href=https://github.com/mntg4u> MN - TG</a>
-◎ Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 3
-◎ Dᴀᴛᴀ Bᴀsᴇ: Mᴏɴɢᴏ DB
-◎ Bᴏᴛ Sᴇʀᴠᴇʀ: KoYeb</b>"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Shobana Filter Bot  is a open source project. 
-- Source - <ahref=https://github.com/mn-bots/ShobanaFilterBot>Click Here to get source code</a>
+    START_TXT = """Hello {}.
+I'm An Auto-filter Bot Which Can Provide Movies In Your Groups.
+○ Add Me To Your Group
+○ Promote Me As Admin To Let Me Get In Action."""
 
-<b>DEVS:</b>
--<a href=https://github.com/mntg4u> MN - TG</a>"""
+
+    HELP_TXT = """Hey {}
+Currently Using A Free Server, So Please Don’t Overload Me..."""
+
+    ABOUT_TXT = """<b>
+○ Creator     : <a href="https://github.com/aktelegram1">MMW BOTZ</a>
+○ Language    : Python 3
+○ Database    : MongoDB
+○ Bot Server  : KoYeb</b>"""
+
+    SOURCE_TXT = """<b>NOTE:</b>
+○ Filter Bot Is Not A Open Source Project"""
+
     MANUELFILTER_TXT = """Help: <b>Filters</b>
-- Filter is the feature were users can set automated replies for a particular keyword and shobana will respond whenever a keyword is found the message
+
+Filter allows users to set automated replies for specific keywords.
+
 <b>NOTE:</b>
-1. This Bot should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+○ Bot must be admin.  
+○ Only admins can add filters.  
+○ Alert buttons have a 64-character limit.
 
 <b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+• /filter – <code>Add a filter</code>  
+• /filters – <code>List all filters</code>  
+• /del – <code>Delete a filter</code>  
+• /delall – <code>Delete all filters (owner only)</code>"""
+
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- This Bot Supports both url and alert inline buttons.
+Supports URL and alert inline buttons.
 
 <b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. This Bot supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+○ Message content is mandatory  
+○ Works with any Telegram media  
+○ Use Markdown formatting properly
 
-<b>URL buttons:</b>
+<b>URL button format:</b>  
 <code>[Button Text](buttonurl:https://github.com/mn-bots/ShobanaFilterBot)</code>
 
-<b>Alert buttons:</b>
+<b>Alert button format:</b>  
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """
 
-<b>ɴᴏᴛᴇ: Fɪʟᴇ Iɴᴅᴇx</b>
-1. ᴍᴀᴋᴇ ᴍᴇ ᴛʜᴇ ᴀᴅᴍɪɴ ᴏꜰ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɪꜰ ɪᴛ'ꜱ ᴘʀɪᴠᴀᴛᴇ.
-2. ᴍᴀᴋᴇ ꜱᴜʀᴇ ᴛʜᴀᴛ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴅᴏᴇꜱ ɴᴏᴛ ᴄᴏɴᴛᴀɪɴꜱ ᴄᴀᴍʀɪᴘꜱ, ᴘᴏʀɴ ᴀɴᴅ ꜰᴀᴋᴇ ꜰɪʟᴇꜱ.
-3. ꜰᴏʀᴡᴀʀᴅ ᴛʜᴇ ʟᴀꜱᴛ ᴍᴇꜱꜱᴀɢᴇ ᴛᴏ ᴍᴇ ᴡɪᴛʜ Qᴜᴏᴛᴇꜱ. ɪ'ʟʟ ᴀᴅᴅ ᴀʟʟ ᴛʜᴇ ꜰɪʟᴇꜱ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴍʏ ᴅʙ.
+    AUTOFILTER_TXT = """<b>Note: File Indexing</b>
 
-<b>Nᴏᴛᴇ: AᴜᴛᴏFɪʟᴛᴇʀ</b>
-1. Aᴅᴅ ᴛʜᴇ ʙᴏᴛ ᴀs ᴀᴅᴍɪɴ ᴏɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ.
-2. Usᴇ /connect ᴀɴᴅ ᴄᴏɴɴᴇᴄᴛ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ ᴛʜᴇ ʙᴏᴛ.
-3. Usᴇ /settings ᴏɴ ʙᴏᴛ's PM ᴀɴᴅ ᴛᴜʀɴ ᴏɴ AᴜᴛᴏFɪʟᴛᴇʀ ᴏɴ ᴛʜᴇ sᴇᴛᴛɪɴɢs ᴍᴇɴᴜ.."""
+○ Make me admin in your channel (if private).  
+○ Channel must not contain camrips, porn, or fake files.  
+○ Forward last message to me with quote — I'll index those files.
+
+<b>Note: AutoFilter</b>
+
+○ Add me as admin in your group.  
+○ Use /connect to link your group.  
+○ Use /settings in PM to enable AutoFilter."""
+
     CONNECTION_TXT = """Help: <b>Connections</b>
 
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
+Allows managing filters from PM, avoiding group spam.
 
 <b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
+○ Only admins can connect.  
+○ Use <code>/connect</code> in group.
 
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
+<b>Commands:</b>
+• /connect – <code>Link group to PM</code>  
+• /disconnect – <code>Unlink a chat</code>  
+• /connections – <code>Show all connections</code>"""
+
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
-<b>NOTE:</b>
-these are the extra features of ShobanaFilterBot
+<b>Commands:</b>
+• /id – <code>Get user ID</code>  
+• /info – <code>Get user info</code>  
+• /imdb – <code>Fetch IMDb info</code>  
+• /search – <code>Search movie from sources</code>  
+• /start – <code>Bot status</code>  
+• /ping – <code>Ping check</code>  
+• /usage – <code>Bot usage</code>  
+• /broadcast – <code>Broadcast (owner only)</code>"""
 
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>
-• /start - <code>Check I'm Alive.</code>
-• /ping - <code>check ping.</code>
-• /usage - <code>usage of bot.</code>
-• /info - <code>User info .</code>
-• /id - <code>User id  .</code>
-• /broadcast - <code>Broadcast (owner only).</code>
-"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
+    ADMIN_TXT = """Help: <b>Admin Mods</b>
 
-<b>NOTE:</b>
-This module only works for my admins
+<b>NOTE:</b> Only bot admins can use these.
 
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
- 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
- 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
- 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 
- 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> """
+<b>Commands:</b>
+• /logs – <code>Recent errors</code>  
+• /stats – <code>File stats</code>  
+• /delete – <code>Delete file</code>  
+• /users – <code>User list</code>  
+• /chats – <code>Chat list</code>  
+• /leave – <code>Leave chat</code>  
+• /disable – <code>Disable chat</code>  
+• /ban – <code>Ban user</code>  
+• /unban – <code>Unban user</code>  
+• /channel – <code>Connected channels</code>  
+• /broadcast – <code>Broadcast message</code>"""
+
+    STATUS_TXT = """★ TOTAL FILES: <code>{}</code>
+TOTAL USERS: <code>{}</code>
+TOTAL CHATS: <code>{}</code>
+USED STORAGE: <code>{}</code>
+FREE STORAGE: <code>{}</code>"""
+
     LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
+Group = {} (<code>{}</code>)
 Total Members = <code>{}</code>
-Added By - {}
-"""
-    RESULT_TXT="""<blockquote> Hey,     </blockquote>
-<blockquote>Jᴜsᴛ Sᴇᴇ Wʜᴀᴛ I Found Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ </blockquote>"""
+Added By - {}"""
 
-    CUSTOM_FILE_CAPTION = """📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
-FɪʟᴇSɪᴢᴇ : {file_size}
+    RESULT_TXT = """<blockquote>Hey,</blockquote>
+<blockquote>Just see what I found for your query.</blockquote>"""
 
-╔═  ᴊᴏɪɴ ᴡɪᴛʜ ᴜs   ═╗
- Jᴏɪɴ :- [MAIN CHANNEL](https://t.me/mn_movies2)
- Jᴏɪɴ :- [Movie Group 1](https://t.me/mn_movies3)
- Jᴏɪɴ :- [Movie Group 2](https://t.me/malayalam_movies_group2)
- Jᴏɪɴ :- [Movie Group 3](https://t.me/Netflix_Group3)
- Jᴏɪɴ :- [Movie Group 4](https://t.me/cinima_theerthadana_kendram)
- Jᴏɪɴ :- [Movie Group 5](https://t.me/malayalam_movies_nbot)
- Jᴏɪɴ :- [Movie Group 6](https://t.me/seriesgroups)
- Jᴏɪɴ :- [Movie Group 7](https://t.me/New_indian_cinemas)
-╚═  ᴊᴏɪɴ ᴡɪᴛʜ ᴜs    ═╝
+    CUSTOM_FILE_CAPTION = """📂 Filename : {file_name}
+FileSize : {file_size}
 
-⚠️ <b>This file will be deleted from here within 1 minute as it has copyright ... !!!</b>
+⚠️ <b>This file will be deleted within 1 minute due to copyright.</b>
 
-<b>കോപ്പിറൈറ്റ് ഉള്ളതുകൊണ്ട് ഫയൽ 1 മിനിറ്റിനുള്ളിൽ ഇവിടെനിന്നും ഡിലീറ്റ് ആകുന്നതാണ് അതുകൊണ്ട് ഇവിടെ നിന്നും മറ്റെവിടെക്കെങ്കിലും മാറ്റിയതിന് ശേഷം ഡൗൺലോഡ് ചെയ്യുക!</b>
-"""
+<b>കോപ്പിറൈറ്റ് ഉള്ളതുകൊണ്ട് ഫയൽ 1 മിനിറ്റിനുള്ളിൽ ഇവിടെനിന്നും ഡിലീറ്റ് ആകുന്നതാണ്. അതിനാൽ, ഫയൽ ഡൗൺലോഡ് ചെയ്യുന്നതിന് മുമ്പ് മറ്റൊരു സ്ഥലത്തേക്ക് മാറ്റുക.</b>"""
 
-    
-    RESTART_GC_TXT = """
-<b>𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 !</b>
+    RESTART_GC_TXT = """<b>Bot Restarted!</b>
 
-📅 𝖣𝖺𝗍𝖾 : <code>{}</code>
-⏰ 𝖳𝗂𝗆𝖾 : <code>{}</code>
-🌐 𝖳𝗂𝗆𝖾𝗓𝗈𝗇𝖾 : <code>Asia/Kolkata</code>
-🛠️ 𝖡𝗎𝗂𝗅𝖽 𝖲𝗍𝖺𝗍𝗎𝗌 : <code>𝗏1 [ 𝖲𝗍able ]</code></b>"""
-    
-    LOG_TEXT_P = """#NewUser
-ID - <code>{}</code>
-Name - {}
-"""
-    SPOLL_NOT_FND="""
-I couldn't find anything related to your request. 
-Try reading the instruction below 
+📅 Date: <code>{}</code>  
+⏰ Time: <code>{}</code>  
+🌐 Timezone: <code>Asia/Kolkata</code>  
+🛠️ Build: <code>v1 [Stable]</code>"""
+
+    LOG_TEXT_P = """#NewUser  
+ID - <code>{}</code>  
+Name - {}"""
+
+    SPOLL_NOT_FND = """
+I couldn't find anything related to your request. Try following the tips below:
+
 <blockquote>
-1️ Ask in Correct Spelling
-2️ Don't ask Movies which are not Realased on OTT PLATFORMS
-3️ Possible  ASK [movie name langauge] like this or [movie year] </blockquote>
-OR
-<b> Tʜɪs Mᴏᴠɪᴇ Is Nᴏᴛ Aᴅᴅᴇᴅ Tᴏ DB</b>
-<pre>Report To ADMIN BY USING /bugs command </pre> 
-    """
-#SPELL CHECK LANGUAGES TO KNOW callback
-    ENG_SPELL="""Please Note Below📓
-1️⃣ Ask in Correct Spelling
-2️⃣ Don't ask Movies which are not Realased on OTT PLATFORMS
-3️⃣ Possible  ASK [movie name langauge] like this or [movie year]
-    """
-    MAL_SPELL="""ദയവായി താഴെ ശ്രദ്ധിക്കുക📓
-1️⃣ ശരിയായ അക്ഷരവിന്യാസത്തിൽ ചോദിക്കുക
-2️⃣ OTT പ്ലാറ്റ്‌ഫോമുകളിൽ റിലീസ് ചെയ്യാത്ത സിനിമകൾ ചോദിക്കരുത്
-3️⃣ ഇത് പോലെ [സിനിമയുടെ പേര് ഭാഷ] അല്ലെങ്കിൽ [സിനിമ വർഷം] ചോദിക്കാം
-    """
-    HIN_SPELL="""कृपया नीचे ध्यान दें📓
-1️⃣ सही वर्तनी में पूछें
-2️⃣ उन फिल्मों के बारे में न पूछें जो ओटीटी प्लेटफॉर्म पर रिलीज नहीं हुई हैं
-3️⃣ संभव है पूछें [मूवी का नाम भाषा] इस तरह या [मूवी वर्ष]
-    """
-    TAM_SPELL="""கீழே கவனிக்கவும்📓
-1️⃣ சரியான எழுத்துப்பிழையில் கேளுங்கள்
-2️⃣ வெளியாகாத திரைப்படங்களைக் கேட்காதீர்கள்
-3️⃣ இந்த வடிவத்தில் கேளுங்கள் [திரைப்படத்தின் பெயர், ஆண்டு]
-    """
+1️ Ask in correct spelling  
+2️ Don’t request unreleased movies  
+3️ Try format: [movie name language] or [movie year]
+</blockquote>
 
-    CHK_MOV_ALRT="""♻️ ᴄʜᴇᴄᴋɪɴɢ ꜰɪʟᴇ ᴏɴ ᴍʏ ᴅᴀᴛᴀʙᴀꜱᴇ... ♻️"""
-    
-    OLD_MES=""" 𝐘𝐨𝐮 𝐚𝐫𝐞 𝐮𝐬𝐢𝐧𝐠 𝐨𝐧𝐞 𝐨𝐟 𝐦𝐲 𝐨𝐥𝐝 𝐦𝐞𝐬𝐬𝐚𝐠𝐞𝐬🤔, 𝐩𝐥𝐞𝐚𝐬𝐞 𝐬𝐞𝐧𝐝 𝐭𝐡𝐞 𝐫𝐞𝐪𝐮𝐞𝐬𝐭 𝐚𝐠𝐚𝐢𝐧"""
-    
-    MOV_NT_FND="""<b>Tʜɪs Mᴏᴠɪᴇ Is Nᴏᴛ Yᴇᴛ Rᴇᴀʟᴇsᴇᴅ Oʀ Aᴅᴅᴇᴅ Tᴏ DB</b>
-<pre>Report To ADMIN BY USING /bugs command </pre> 
-"""
-    RESTART_TXT = """
-<b><u>𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 ✅</u></b>"""
+<b>This movie is not added to the database yet.</b>
+<pre>Report to admin using /bugs</pre>"""
 
+    ENG_SPELL = """Please Note:  
+1️⃣ Ask in correct spelling  
+2️⃣ Don’t ask for unreleased movies  
+3️⃣ Use format: [movie name language] or [movie year]"""
+
+    MAL_SPELL = """ദയവായി ശ്രദ്ധിക്കുക:  
+1️⃣ ശരിയായ അക്ഷരവിന്യാസം  
+2️⃣ റിലീസ് ചെയ്യാത്ത സിനിമകൾ ചോദിക്കരുത്  
+3️⃣ [പേര് ഭാഷ] അല്ലെങ്കിൽ [വർഷം] പോലെ ചോദിക്കുക"""
+
+    HIN_SPELL = """कृपया ध्यान दें:  
+1️⃣ सही वर्तनी  
+2️⃣ रिलीज़ न हुई फ़िल्में न पूछें  
+3️⃣ [नाम भाषा] या [साल] जैसे पूछें"""
+
+    TAM_SPELL = """குறிப்பு:  
+1️⃣ சரியான எழுத்துப்பிழை  
+2️⃣ வெளியான படங்களைக் கேளுங்கள்  
+3️⃣ [பட பெயர், ஆண்டு] போல கேளுங்கள்"""
+
+    CHK_MOV_ALRT = """Checking file in my database..."""
+
+    OLD_MES = """You're using an old message. Please resend the request."""
+
+    MOV_NT_FND = """<b>This movie is not yet released or added to the database.</b>  
+<pre>Report to admin using /bugs</pre>"""
+
+    RESTART_TXT = """<b><u>Bot Restarted ✅</u></b>"""
